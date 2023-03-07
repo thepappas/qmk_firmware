a7ee34cfdee1464ad07276728cb86d0dd08e0591
John Pappas <git@jvpappas.net>
2023-03-07 02:33:22 -0700

    Initial crkbd layout - Autoshift enabled - Np alt or ctl yet - Basic OLED layer indicatot

---
+++ keyboards/crkbd/keymaps/thepappas/rules.mk
@@ -0,0 +1,7 @@
+MOUSEKEY_ENABLE = yes    # Mouse keys
+RGBLIGHT_ENABLE = yes    # Enable WS2812 RGB underlight.
+OLED_ENABLE     = yes
+OLED_DRIVER     = SSD1306
+LTO_ENABLE      = yes
+VIA_ENABLE		= yes
+AUTO_SHIFT_ENABLE = yes
