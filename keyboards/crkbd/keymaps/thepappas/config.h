3a9be4d9a474c3534a44cb105374718353a627b5
John Pappas <gitlab@jvpappas.net>
2023-06-09 13:59:55 -0600

    Disable backlight

--- keyboards/crkbd/keymaps/thepappas/config.h
+++ keyboards/crkbd/keymaps/thepappas/config.h
@@ -24,7 +24,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define MASTER_RIGHT
 // #define EE_HANDS
 
-#ifdef RGBLIGHT_DISABLE
+#ifdef RGBLIGHT_ENABLE
     #define RGBLIGHT_EFFECT_BREATHING
     #define RGBLIGHT_EFFECT_RAINBOW_MOOD
     #define RGBLIGHT_EFFECT_RAINBOW_SWIRL
--- keyboards/crkbd/keymaps/thepappas/rules.mk
+++ keyboards/crkbd/keymaps/thepappas/rules.mk
@@ -1,5 +1,5 @@
 MOUSEKEY_ENABLE = yes    # Mouse keys
-RGBLIGHT_ENABLE = yes    # Enable WS2812 RGB underlight.
+RGBLIGHT_ENABLE = no     # Enable WS2812 RGB underlight.
 OLED_ENABLE     = yes
 OLED_DRIVER     = SSD1306
 LTO_ENABLE      = yes
