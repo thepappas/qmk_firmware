101ca3e87d4d4f746f5823b68f5f376aa5d52f13
John Pappas <gitlab@jvpappas.net>
2023-07-02 14:40:07 -0600

    Thepappas: Add F13-24

--- keyboards/crkbd/keymaps/thepappas/keymap.c
+++ keyboards/crkbd/keymaps/thepappas/keymap.c
@@ -37,9 +37,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                              KC_TRNS, KC_TRNS, KC_TRNS,              KC_TRNS, KC_TRNS, KC_TRNS
     ),
 	[FN] = LAYOUT_split_3x6_3(
-        KC_LCTL, KC_F12, KC_F7, KC_F8, KC_F9, KC_NO,     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_RCTL,
-        KC_LALT, KC_F11, KC_F4, KC_F5, KC_F6, KC_NO,     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_RALT,
-        SC_LSPO, KC_F10, KC_F1, KC_F2, KC_F3, KC_NO,     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_RSFT,
+        KC_LCTL, KC_F12, KC_F7, KC_F8, KC_F9, KC_NO,     KC_NO, KC_F17, KC_F18, KC_F19,  KC_NO, KC_RCTL,
+        KC_LALT, KC_F11, KC_F4, KC_F5, KC_F6, KC_NO,     KC_NO, KC_F14, KC_F15, KC_F16, KC_F24, KC_RALT,
+        SC_LSPO, KC_F10, KC_F1, KC_F2, KC_F3, KC_NO,     KC_NO, KC_F21, KC_F22, KC_F13, KC_F23, KC_RSFT,
                           KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS
     ),
     [Nav] = LAYOUT_split_3x6_3(
