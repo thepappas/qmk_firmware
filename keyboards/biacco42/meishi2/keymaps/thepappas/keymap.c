77edf64e339a4e9ccf066948254442d687d44bb6
John Pappas <gitlab@jvpappas.net>
2023-07-17 10:42:22 -0600

    Thepappas; Meishi2 - Add LED Control code - Add additional layers/LTs

--- keyboards/biacco42/meishi2/keymaps/thepappas/keymap.c
+++ keyboards/biacco42/meishi2/keymaps/thepappas/keymap.c
@@ -16,6 +16,9 @@
 #include QMK_KEYBOARD_H
 #include "thepappas.h"
 
+#define PRO_MICRO_LED_TX D5
+#define PRO_MICRO_LED_RX B0
+
 enum layer_names {
     Base,
     Macro,
@@ -25,15 +28,26 @@ enum layer_names {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [Base] = LAYOUT( /* Base */
-    KVM1, KVM2, KC_WAKE, KC_SLEP
+    KVM1, KVM2, LT(Macro,LGUI(KC_L)), LT(FN,LCA(KC_DEL))
   ),
   [Macro] = LAYOUT(
-    LCTL(KC_Z),  LCTL(KC_X),  LCTL(KC_C), LCTL(KC_V)
+    LCTL(KC_V), LCTL(KC_C), KC_TRNS, LCTL(KC_Z)
   ),
   [FN] = LAYOUT(
-    KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS
+      KC_WAKE, KC_SLEP, KC_PWR, KC_TRNS
   ),
   [Nav] = LAYOUT(
     KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS
   ),
 };
+
+void matrix_init_user(void) {
+  setPinOutput(PRO_MICRO_LED_TX);
+  setPinOutput(PRO_MICRO_LED_RX);
+}
+
+bool led_update_user(led_t led_state) {
+  writePin(PRO_MICRO_LED_TX, !led_state.num_lock);
+  writePin(PRO_MICRO_LED_RX, !led_state.caps_lock);
+  return true;
+}
